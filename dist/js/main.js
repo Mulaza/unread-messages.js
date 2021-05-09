3cb0a0de3f2414870e56d5b40cc1505051c4bfa2
Mulaza <mulazajacinto@Gmail.com>
2021-05-09 21:42:39 +0800

    updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,9 +7,10 @@
 
         let position = item.getAttribute('data-pos');
         let color = item.getAttribute('data-color');
+        let click_state = item.getAttribute('data-select-hide');
 
 
-        // position
+        // Position
         switch (position) {
             case "top":
                 circ.style.top = "-10px";
@@ -46,15 +47,19 @@
                 break;
         }
 
+        if (!['top', 'top-right', 'top-left', 'bottom', 'bottom-right', 'bottom-left', null].includes(position)) {
+            console.error('Unsupported position ', position);
+        }
 
-        // color
+
+        // Color
         if (color === "") {
             circ.style.backgroundColor = "red";
         }
         circ.style.backgroundColor = color;
 
 
-        // data
+        // Data
         if (item.getAttribute('data-val') >= 1000 && item.getAttribute('data-val') < 100000) {
             circ.innerHTML = `${(item.getAttribute('data-val') / 1000).toFixed(2)}K`;
         } else if (item.getAttribute('data-val') >= 100000) {
@@ -62,14 +67,44 @@
         }
         else {
             circ.innerHTML = item.getAttribute('data-val'); // check if is a number
-
         }
 
 
+        // // Max Value displayed
+        // if (item.getAttribute('data-max-val') != null) {
+        //     let MAX_VALUE = parseInt(item.getAttribute('data-max-val'));
+        //     console.log(parseInt(item.getAttribute('data-max-val')), 'was defined');
+        // }
+
+
+        // Touch Listener 
+        function shrink() {
+            circ.classList.add('circle-shrink');
+        };
+
+        if (click_state == null || click_state === "true" || click_state === "True") {
+            item.addEventListener('click', shrink);
+        } else if (click_state == 'false' || click_state == 'False') {
+            // No need to add the event listener
+        }
+
+        if (!['true', 'false', 'True', 'False', null].includes(click_state)) {
+            console.error('Unsupported value: ', click_state)
+        }
+
+
+
+
+        // Default number value
         if (item.getAttribute('data-val') == "") {
             item.setAttribute('data-val', '0');
             circ.innerHTML = item.getAttribute('data-val');
         }
+
+
+
+
+
 
 
 
